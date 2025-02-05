7088ad7bb30f3c374435e60a1a25b5361e10c20d
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-05 11:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//your JS code here. If required.
-let year=prompt("enter year")
+let year = prompt("Enter a year");
 
-if ((year%4==0&&year%100==0)||(year%400!=0)) {
-	alert("366")
-	
-}
-else{
-	return alert("365")
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    alert("366");
+} else {
+    alert("365");
 }
