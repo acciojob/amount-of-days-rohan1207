e4ff3c34bbf340413138db20a59d82aacdfabd0e
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-06 21:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function daysOfAYear(year) {
     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-        return 366; // Leap year
+        return 366;
     } else {
-        return 365; // Non-leap year
+        return 365; 
     }
 }
